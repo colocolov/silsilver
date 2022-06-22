aa2d5d5e04bd1ce28c4d46e1b22d4f2d93ea5864
Stas Colocolov <colocolov.stas@gmail.com>
2022-06-22 23:09:32 +0300

    add riews

--- #src/js/scripts/reviews.js
+++ #src/js/scripts/reviews.js
@@ -1,15 +1,21 @@
 // // слайдер отзывов
-// var reviewsSlider = new Swiper(".reviews__sliders", {
-//   loop: true,
-//   navigation: {
-//     nextEl: ".reviews-button--right",
-//     prevEl: ".reviews-button--left",
-//     clickable: true,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     type: "bullets",
-//     clickable: true,
-//   },
-// });
-// //----- END
+let reviewsSlider = new Swiper(".reviews__sliders", {
+  // loop: true,
+  // autoplay: {
+  //   delay: 3000,
+  // },
+  // speed: 800,
+  // direction: "vertical",
+  //
+  // navigation: {
+  //   nextEl: ".reviews-button--right",
+  //   prevEl: ".reviews-button--left",
+  //   clickable: true,
+  // },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   type: "bullets",
+  //   clickable: true,
+  // },
+});
+//----- END
