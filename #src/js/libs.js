82d94f0950f0098819d911f6fb8f03b40eea1cfd
Stas Colocolov <colocolov.stas@gmail.com>
2022-06-30 23:44:07 +0300

    fix bugs in nav, links

--- #src/js/libs.js
+++ #src/js/libs.js
@@ -1,1 +1,1 @@
-import "../../node_modules/swiper/swiper-bundle.js";
+// import "../../node_modules/swiper/swiper-bundle.js";
--- #src/js/scripts/nav.js
+++ #src/js/scripts/nav.js
@@ -1,23 +1,23 @@
 // мобильное меню
 
 //выпадающее меню для мобильных устройств
-document.addEventListener("click", documentActions);
+// document.addEventListener("click", documentActions);
 
-function documentActions(e) {
-  const targetElement = e.target;
-  if (window.innerWidth > 900) {
-    if (targetElement.classList.contains("menu__arrow")) {
-      targetElement.closest(".menu__item").classList.toggle("_hover");
-    }
-    if (
-      !targetElement.closest(".menu__item") &&
-      document.querySelectorAll(".menu__item._hover").length > 0
-    ) {
-      // console.log("ehhh");
-      // _removeClasses(document.querySelectorAll(".menu__item._hover"), "_hover");
-    }
-  }
-}
+// function documentActions(e) {
+//   const targetElement = e.target;
+//   if (window.innerWidth > 900) {
+//     if (targetElement.classList.contains("menu__arrow")) {
+//       targetElement.closest(".menu__item").classList.toggle("_hover");
+//     }
+//     if (
+//       !targetElement.closest(".menu__item") &&
+//       document.querySelectorAll(".menu__item._hover").length > 0
+//     ) {
+//       // console.log("ehhh");
+//       // _removeClasses(document.querySelectorAll(".menu__item._hover"), "_hover");
+//     }
+//   }
+// }
 
 // проверка, является ли уствройство мобильным
 const isMobile = {
@@ -55,7 +55,7 @@ if (isMobile.any()) {
 // меню бургер
 const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
-const menuLink = document.querySelector(".menu__item");
+const menuLink = document.querySelectorAll(".menu__item");
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
     document.body.classList.toggle("_lock");
@@ -64,9 +64,11 @@ if (iconMenu) {
   });
 }
 // закрытие при клике
-menuLink.addEventListener("click", function (e) {
-  console.log("yes");
-  document.body.classList.remove("_lock");
-  iconMenu.classList.remove("_active");
-  menuBody.classList.remove("_active");
+menuLink.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("yes");
+    document.body.classList.remove("_lock");
+    iconMenu.classList.remove("_active");
+    menuBody.classList.remove("_active");
+  });
 });
