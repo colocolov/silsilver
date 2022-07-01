6820fa0fab7bc6f482d06408f2219a6933dedc1c
Stas Colocolov <colocolov.stas@gmail.com>
2022-07-02 00:44:21 +0300

    upd slider reviews

--- #src/js/scripts/reviews.js
+++ #src/js/scripts/reviews.js
@@ -1,16 +1,23 @@
 // // слайдер отзывов
 let reviewsSlider = new Swiper(".reviews__sliders", {
-  // loop: true,
+  loop: true,
+  // slidesPerView: 1,
+  // spaceBetween: 30,
+  // centeredSlides: true,
   // autoplay: {
   //   delay: 3000,
   // },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
   // speed: 800,
   // direction: "vertical",
   //
   // navigation: {
-  //   nextEl: ".reviews-button--right",
-  //   prevEl: ".reviews-button--left",
-  //   clickable: true,
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  //   // clickable: true,
   // },
   // pagination: {
   //   el: ".swiper-pagination",
